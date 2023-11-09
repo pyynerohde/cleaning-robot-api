7b089a51cd2800b684ee39883aafda91fd2b638c
Oscar Rohde <oscar.rohde@sas.se>
2023-11-09 22:47:24 +0100

    init

--- CleaningRobotApi/Data/Execution.cs
+++ CleaningRobotApi/Data/Execution.cs
@@ -1,1 +1,15 @@
-// Define an entity that corresponds to the executions table.
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace tibber_robot_api.Data
+{
+    public class Execution
+    {
+        [Key]
+        public int Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public int Commands { get; set; }
+        public int Result { get; set; }
+        public double Duration { get; set; }
+    }
+}
