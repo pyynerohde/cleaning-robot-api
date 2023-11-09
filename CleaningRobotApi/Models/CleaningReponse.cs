7a527db538154a9e028fcf16bc49045d132a4d30
Oscar Rohde <oscar.rohde@sas.se>
2023-11-09 22:51:14 +0100

    add comment, do we need this much?

--- CleaningRobotApi/Models/CleaningReponse.cs
+++ CleaningRobotApi/Models/CleaningReponse.cs
@@ -1,11 +1,11 @@
-// Represents the result of the cleaning operation
-
 using System; 
 
 namespace TibberCleaningRobotApi.Models
 {
     public class CleaningResponse
     {
+        // Is it uneccessary to have Id, Timestamp, Command, and Duration? 
+        // We don't return anything else than UniquePlacesCleaned in the response atm
         public int Id { get; set; }
         public DateTime Timestamp { get; set; }
         public int Commands { get; set; }
