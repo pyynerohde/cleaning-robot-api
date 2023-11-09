de00fb2d7aa1af4c6b254150da05962eaafd8c5e
Oscar Rohde <oscar.rohde@sas.se>
2023-11-09 23:01:33 +0100

    configure AppDbContext and dependency injections

--- CleaningRobotApi/Program.cs
+++ CleaningRobotApi/Program.cs
@@ -1,11 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICleaningRobotService, CleaningRobotService>();
 
 var app = builder.Build();
 
@@ -17,9 +20,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
