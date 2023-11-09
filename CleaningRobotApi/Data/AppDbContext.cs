1de17cb2dd57ed756d3ed717e02c5481146a76aa
Oscar Rohde <oscar.rohde@sas.se>
2023-11-09 22:46:57 +0100

    init, use env variable for connection string

--- CleaningRobotApi/Data/AppDbContext.cs
+++ CleaningRobotApi/Data/AppDbContext.cs
@@ -1,2 +1,30 @@
-// Create a new class that inherits from DbContext and configure it to connect to PostgreSQL.
+using Microsoft.EntityFrameworkCore;
 
+namespace tibber_robot_api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Execution> Executions { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Execution>()
+                .Property(p => p.Id)
+                .ValueGeneratedOnAdd();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseNpgsql(Environment.GetEnvironmentVariable("DB_CONNECTION_STRING"))
+                              .UseSnakeCaseNamingConvention();
+            }
+        }
+    }
+}
