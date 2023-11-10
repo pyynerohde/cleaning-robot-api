f29372ba0ed306bb415624d8562b4c17fbdc8424
Oscar Rohde <oscar.rohde@sas.se>
2023-11-10 13:32:31 +0100

    apply db migration at runtime

--- CleaningRobotApi/Program.cs
+++ CleaningRobotApi/Program.cs
@@ -20,6 +20,12 @@ builder.Services.AddScoped<ICleaningRobotService, CleaningRobotService>();
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
