b57dca628771b0a054fe3c1f7ca6b480936b4adb
Oscar Rohde <oscar.rohde@sas.se>
2023-11-10 13:42:51 +0100

    these are not necessary as part of the response

--- CleaningRobotApi/Models/CleaningReponse.cs
+++ CleaningRobotApi/Models/CleaningReponse.cs
@@ -6,10 +6,10 @@ namespace TibberCleaningRobotApi.Models
     {
         // Is it uneccessary to have Id, Timestamp, Command, and Duration? 
         // We don't return anything else than UniquePlacesCleaned in the response atm
-        public int Id { get; set; }
-        public DateTime Timestamp { get; set; }
-        public int Commands { get; set; }
+//        public int Id { get; set; }
+//        public DateTime Timestamp { get; set; }
+//        public int Commands { get; set; }
         public int UniquePlacesCleaned { get; set; }
-        public double Duration { get; set; }
+//        public double Duration { get; set; }
     }
 }
