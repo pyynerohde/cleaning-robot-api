78ed26831fafe70d6cade13c33723661da2d857c
Oscar Rohde <oscar.rohde@sas.se>
2023-11-10 10:28:52 +0100

    add dependencies

--- CleaningRobotApi/Program.cs
+++ CleaningRobotApi/Program.cs
@@ -1,3 +1,11 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using TibberCleaningRobotApi.Data; 
+using TibberCleaningRobotApi.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
